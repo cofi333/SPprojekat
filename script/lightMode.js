a274b147cb1dd74a590185035ceeb4925483c2b4
Filip Kujundzic <filipkujundzic3@gmail.com>
2023-02-13 20:23:26 +0100

    13.feb

--- script/lightMode.js
+++ script/lightMode.js
@@ -3,12 +3,11 @@ function lightFunction() {
     const hero2 = document.getElementById("hero2");
     const products = document.getElementById("products");
     const aboutUs = document.getElementById("aboutUs");
-    const hero = document.getElementById("hero-text");
+
     
     workTime.classList.toggle("lightMode");
     hero2.classList.toggle("lightMode");
     products.classList.toggle("lightMode");
     aboutUs.classList.toggle("lightMode");
-    hero.classList.toggle("lightMode");
 
   }
