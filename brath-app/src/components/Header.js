69c95a5cebc9a8a671038a431b7e0049eb86d5a4
Filip Kujundzic <filipkujundzic3@gmail.com>
2023-02-15 22:59:45 +0100

    15.feb

--- brath-app/src/components/Header.js
+++ brath-app/src/components/Header.js
@@ -8,12 +8,19 @@ function Header() {
 
   function myFunction() {
     var x = document.getElementById("navigationElements");
+    var y = document.querySelectorAll("li");
     if (x.style.display === "block") {
       x.style.display = "none";
     } else {
       x.style.display = "block";
     }
+
+    y.forEach(n => n.addEventListener("click", () => {
+      x.style.display="none";
+      
+    }))
   }
+
   
   return (
     <header id="navigation">
--- script/hamburgerMenu.js
+++ script/hamburgerMenu.js
@@ -1,9 +1,15 @@
 function myFunction() {
     var x = document.getElementById("navigationElements");
+    var y = document.querySelectorAll("li");
     if (x.style.display === "block") {
       x.style.display = "none";
     } else {
       x.style.display = "block";
     }
+
+    y.forEach(n => n.addEventListener("click", () => {
+      x.style.display="none";
+      
+    }))
   }
 
